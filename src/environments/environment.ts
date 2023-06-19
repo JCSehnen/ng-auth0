a442281312858570c5503b91a52da020f192e98a
JCSehnen <josesehnen@gmail.com>
2023-06-19 20:37:54 -0300

    environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  auth0: {
+    domain: 'YOUR_AUTH0_DOMAIN',
+    clientId: 'YOUR_AUTH0_CLIENT_I',
+    redirectUri: window.location.origin,
+  },
+};
